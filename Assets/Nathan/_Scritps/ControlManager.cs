1f4a9c8c7860b9d71694ea50005a4081e9605aa2
Bonkena <nath.bonkena@gmail.com>
2019-09-30 16:48:50 +0200

    Camera Orbit + Camera Controller Switch
    switcher entre le swipe pour controler le vehicule et le swipe pour controller la camera

---
+++ Assets/Nathan/_Scritps/ControlManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlManager : MonoBehaviour
+{
+    public PlayerMovementScript player;
+    public CameraController cam;
+    public bool camControl = false;
+
+    public void SwitchControl()
+    {
+        if (!camControl)
+        {
+            camControl = true;
+        }
+        else if (camControl)
+        {
+            camControl = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (!camControl)
+        {
+            cam.enabled = false;
+            player.enabled = true;
+        }
+        else if (camControl)
+        {
+            cam.enabled = true;
+            player.enabled = false;
+        }
+    }
+}
