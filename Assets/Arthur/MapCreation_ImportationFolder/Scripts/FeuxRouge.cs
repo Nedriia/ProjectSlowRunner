8f8abfa91f844590c7ba81f2761782e5747d1517
P4nzerIV <igor2paris@gmail.com>
2019-11-21 21:15:16 +0100

    Light on lightpost
    Light now work on lightpost

--- Assets/Arthur/MapCreation_ImportationFolder/Scripts/FeuxRouge.cs
+++ Assets/Arthur/MapCreation_ImportationFolder/Scripts/FeuxRouge.cs
@@ -6,17 +6,31 @@ public class FeuxRouge : MonoBehaviour
 {
     public float timer;
     public float timerTot;
+    public GameObject Trafficlight;
+    public Material RedM;
+    public Material BlueM;
 
     public bool red;
+    private void Start()
+    {
+        if (red==true) Trafficlight.GetComponent<Renderer>().material = RedM;
+        else Trafficlight.GetComponent<Renderer>().material = BlueM;
+    }
 
     void Update()
     {
         timer += Time.deltaTime;
         if(timer > timerTot){
             if (red)
+            {
                 red = false;
+                Trafficlight.GetComponent<Renderer>().material = BlueM;
+            }
             else
+            {
                 red = true;
+                Trafficlight.GetComponent<Renderer>().material = RedM;
+            }
             timer = 0;
         }
     }
