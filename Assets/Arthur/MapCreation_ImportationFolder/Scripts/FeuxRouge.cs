b36bfee8b85d1f562e00fdc6fd1229f66492dd68
Nedriia <arthur.favre@hotmail.com>
2019-11-21 21:31:07 +0100

    Resolve Feux Rouge

--- Assets/Arthur/MapCreation_ImportationFolder/Scripts/FeuxRouge.cs
+++ Assets/Arthur/MapCreation_ImportationFolder/Scripts/FeuxRouge.cs
@@ -6,30 +6,32 @@ public class FeuxRouge : MonoBehaviour
 {
     public float timer;
     public float timerTot;
-    public GameObject Trafficlight;
+    public Renderer Trafficlight;
     public Material RedM;
     public Material BlueM;
 
     public bool red;
     private void Start()
     {
-        if (red==true) Trafficlight.GetComponent<Renderer>().material = RedM;
-        else Trafficlight.GetComponent<Renderer>().material = BlueM;
+        if (Trafficlight != null)
+        {
+            if (red == true) Trafficlight.material = RedM;
+            else Trafficlight.material = BlueM;
+        }
     }
 
     void Update()
     {
         timer += Time.deltaTime;
         if(timer > timerTot){
-            if (red)
-            {
-                red = false;
-                Trafficlight.GetComponent<Renderer>().material = BlueM;
-            }
-            else
-            {
-                red = true;
-                Trafficlight.GetComponent<Renderer>().material = RedM;
+            if (Trafficlight != null){
+                if (red){
+                    red = false;
+                    Trafficlight.material = BlueM;
+                }else{
+                    red = true;
+                    Trafficlight.material = RedM;
+                }
             }
             timer = 0;
         }
