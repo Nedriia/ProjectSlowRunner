7d7bbb973f850ca05bca5abb6413e172b836211c
Nathzn <nath.bonkena@gmail.com>
2019-11-22 02:39:37 +0100

    PerspectivePan
    Change PerspectivePan

--- Assets/Scripts/PerspectivePan.cs
+++ Assets/Scripts/PerspectivePan.cs
@@ -6,6 +6,8 @@ public class PerspectivePan : MonoBehaviour
 {
     private Vector3 touchStart;
     public Camera cam;
+    public Vector3 boundsMax;
+    public Vector3 boundsMin;
     public float groundZ = 0;
 
     private void Update()
@@ -18,13 +20,14 @@ public class PerspectivePan : MonoBehaviour
         {
             Vector3 direction = touchStart - GetWorldPosition(groundZ);
             Camera.main.transform.position += direction;
+            //Camera.main.transform.position = new Vector3(Mathf.Clamp(Camera.main.transform.position.x, boundsMin.x, boundsMax.x), Mathf.Clamp(Camera.main.transform.position.y, boundsMin.y, boundsMax.y));
         }
     }
 
     private Vector3 GetWorldPosition(float z)
     {
         Ray mousePos = cam.ScreenPointToRay(Input.mousePosition);
-        Plane ground = new Plane(Vector3.forward, new Vector3(0, 0, z));
+        Plane ground = new Plane(Vector3.up, new Vector3(0, 0, z));
         float distance;
         ground.Raycast(mousePos, out distance);
         return mousePos.GetPoint(distance);
