9ffb0011e6e78772c99e7fb1e89f91d8c46f1f11
Dranak <robin.lacomblez@gmail.com>
2020-01-27 22:19:40 +0100

    Revert "Vroum Vroum version"
    This reverts commit ab01d99c7b00330a44988b0cc2f1168800fcc1ac.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float offsetX, offsetY, offsetZ;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position;
+        transform.rotation = Quaternion.LookRotation(target.up, target.forward);
+
+        transform.position -= (transform.rotation * offset);
+    }
+}
---
+++ Assets/Scripts/OrthographicPan.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrthographicPan : MonoBehaviour
+{
+    Vector3 touchStart;
+    public float zoomOutMin = 1;
+    public float zoomOutMax = 8;
+    public Vector3 boundsMax;
+    public Vector3 boundsMin;
+    public bool isPanning;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isPanning = true;
+            touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if(Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+            float difference = currentMagnitude - prevMagnitude;
+
+            zoom(difference * 0.01f);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Camera.main.transform.position += direction;
+            Camera.main.transform.position = new Vector3(Mathf.Clamp(Camera.main.transform.position.x, boundsMin.x, boundsMax.x), Mathf.Clamp(Camera.main.transform.position.y, boundsMin.y, boundsMax.y));
+        }
+
+        if (!Input.GetMouseButton(0))
+        {
+            isPanning = false;
+        }
+
+        zoom(Input.GetAxis("Mouse ScrollWheel"));
+
+        
+    }
+
+    void zoom(float increment)
+    {
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, zoomOutMin, zoomOutMax);
+    }
+}
