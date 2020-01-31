a0899947388391e1ff6df5b4f3cf6b891a1fb38c
Dranak <robin.lacomblez@gmail.com>
2020-01-31 03:40:34 +0100

    Vous avez dis Feed Back
    Vous avez dis Feed Back ?

---
+++ Assets/Robin/Script/FeedBack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class FeedBack : MonoBehaviour
+{
+    public List<GameObject> Effects;
+
+
+ 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Effects.ForEach(e => e.SetActive(true));
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Effects.ForEach(e => e.SetActive(false));
+    }
+}
---
+++ Assets/Robin/Script/Glitch.cs
@@ -0,0 +1,15 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glitch : MonoBehaviour
+{
+    public float duration = 1.5f;
+    public float force = .4f;
+
+    private void OnEnable()
+    {
+        Camera.main.transform.DOShakePosition(duration, force);
+    }
+}
